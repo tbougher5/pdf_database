649e91fbe618093488f4343eca13ded992546d7d
tbougher5 <60268319+tbougher5@users.noreply.github.com>
2023-12-08 08:49:45 -0600

    Add files via upload

--- pdf_database_streamlit.py
+++ pdf_database_streamlit.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import pickle
+#import pickle
 
 prdDFlist = [['All Products','DF_All_Products_v6B.sav'], ['Architectural Windows', 'DF_Architectural_Windows_v4.sav'], ['Curtain Wall', 'DF_Curtain_Wall_v4.sav']\
              ,['Entrances','DF_Entrances_v4.sav'],['Storefront','DF_Storefront_v4.sav'],['Window Wall','DF_Window_Wall_v4.sav'],['Sun Controls', 'DF_Sun_Controls_v4.sav']]
@@ -10,6 +10,8 @@ prdNum = 2
 
 bolList = []
 dtlList = []
+
+dfFile = 'DF_All_Products_v6B.sav'
 
 def convert_df(df):
     
@@ -38,7 +40,6 @@ def run_search():
     #    if prdCat in prdDFlist[i][0]:
     #        dfFile = prdDFlist[i][1]
     #        break'
-    dfFile = 'DF_All_Products_v6B.sav'
 
     #cols = ['Product Name','Full Name','Secondary Heading','Filename','Detail Heading 1','Detail Heading 2','Page Number','Search String']
     #workbook = xlsxwriter.Workbook(fOut)
@@ -54,16 +55,28 @@ def run_search():
     #print(prd)
     if prdCat == 'All Products':
       if st.session_state.hypStr == True:
-        dfOut = dfPrd.loc[(dfPrd['Text'].str.contains('|'.join([txtStr, txtStr2, txtStr3]),case=False, na=False, regex=True))]
+        dfOut1 = dfPrd.loc[(dfPrd['Text'].str.contains('|'.join([txtStr, txtStr2, txtStr3]),case=False, na=False, regex=True))]
       else:
-        dfOut = dfPrd.loc[(dfPrd['Text'].str.contains(txtStr,case=False, na=False, regex=True))]
+        dfOut1 = dfPrd.loc[(dfPrd['Text'].str.contains(txtStr,case=False, na=False, regex=True))]
     else:
-        dfOut = dfPrd.loc[(dfPrd['Text'].str.contains(txtStr,case=False, na=False, regex=True)) & (dfPrd['Product Category'] == prdCat)]
+        dfOut1 = dfPrd.loc[(dfPrd['Text'].str.contains(txtStr,case=False, na=False, regex=True)) & (dfPrd['Product Category'] == prdCat)]
+
+    if st.session_state.actPrd == True:
+      dfOut2 = dfOut1.loc[(dfPrd['Status'] == 'Active')]
+    else:
+      dfOut2 = dfOut1
+    if not docList:
+      dfOut = dfOut2
+    else:
+      dfOut = dfOut2[dfOut2['Document Type'].isin(docList)]
 
     dfOut['Count'] = dfOut['Count'].astype('int')
-    cols = ['Product Category','Document Type','Status','Document Info #1','Document Info #2','Filename','Page Info #1','Page Info #2','Page #','Text','Filepath']
-    df3 = dfOut.groupby('Product Name').sum().drop(columns = cols)
-    df4 = dfOut.groupby('Filename').sum().drop(columns = 'Page #')
+    print(list(dfOut.columns.values))
+    cols1 = ['Product Category','Document Type','Status','Document Info #1','Document Info #2','Filename','Page Info #1','Page Info #2','Page #','Text','Filepath']
+    cols2 = ['Product Name','Product Category','Document Type','Status','Document Info #1','Document Info #2','Page Info #1','Page Info #2','Page #','Text','Filepath']
+    #cols = ['Product Category','Product Description','Filename','Page Title','Page Description','Page Description 2','Page Number']
+    df3 = dfOut.groupby('Product Name').sum().drop(columns = cols1)
+    df4 = dfOut.groupby('Filename').sum().drop(columns = cols2)
     
     #dfLine = pd.DataFrame(columns = cols)
     #dfOut.to_csv(fOut)
@@ -80,9 +93,15 @@ def run_search():
         col1.dataframe(df3)
         col1.header('Number of results by file')
         col1.dataframe(df4)
-        col1.text(txtStr)
 # closing the pdf file object
 
+def list_files():
+  dfP = pd.read_pickle(dfFile)
+  dfOut = dfP['Filename'].drop_duplicates()
+  #dfOut = dfOut.sort_values
+  with st.container():
+    col1edge, col1, col2edge = st.columns((1, 12, 1))
+    col1.dataframe(dfOut)
 # filePath is a string that contains the path to the pdf
 ct = 0
 
@@ -92,8 +111,8 @@ with st.container():
     col1edge, col1, col2edge = st.columns((1, 12, 1))
     col1.title('Web Details Parts Search')
     col1.caption('')
-    col1.caption('Contains web details, installation manuals, and Florida Product Approvals')
-    col1.caption('Last Updated: 12/3/23')
+    col1.caption('Contains web details, installation manuals, structural charts, and Florida Product Approvals')
+    col1.caption('Last Updated: 12/7/23')
     col1.caption('')
 
 with st.container():
@@ -105,11 +124,27 @@ with st.container():
     col1.text('')
     col2.text('')
     col3.text('')
-    col1.checkbox('Exact part only (e.g., GP-100 but not GP-1001)', key='exStr', value=True)
-    col2.checkbox('Include results without hyphen or space', key='hypStr', value=False)
+    
     col1.text('')
-    col1.button(label='Calculate', key ='calc')
+    col2.text('')
+    col3.text('')
+    col1.checkbox('Active products only',key='actPrd', value=True)
+    col2.checkbox('Exact part only (e.g., GP-100 but not GP-1001)', key='exStr', value=True)
+    col3.checkbox('Include results without hyphen or space', key='hypStr', value=False)
+    
     #col1.button_calc = st.button(label='Calculate', key ='calc')
+    with st.container():
+    #col1, col2 = st.columns(2)
+      col1edge, col1, col2edge = st.columns((1, 9, 1))
+      docList = col1.multiselect('Types of documents to search:',['Web Details','Installation Manuals','Florida Product Approvals', 'Sell Sheet', 'Specifications', 'Structural Charts'],[])
+      col1.text('')
+      col1.button(label='Search', key ='calc')
+      col1.text('')
+      col1.button(label='List Included Files', key ='fileList')
+
 
 if st.session_state.calc:
     run_search()
+
+if st.session_state.fileList:
+    list_files()
---
+++ streamlit_pdf_updater.py
@@ -0,0 +1,166 @@
+from importlib.metadata import files
+from operator import lshift
+import os
+import pandas as pd
+import numpy as np
+import xlsxwriter
+import fitz
+import time
+
+tic = time.perf_counter()
+
+#====================================
+#------Typical Items to Edit---------
+pdfDir = "E:\STREAMLIT"
+prd1 = 'All Parts'
+fName = 'DF_All_Products_Test'
+#---write a pickle (.sav) file in addition to xlsx
+wrtPkl = True
+#======================================
+
+fOut = fName + ".xlsx"
+workbook = xlsxwriter.Workbook(fOut)
+worksheet = workbook.add_worksheet(prd1) 
+#cols = ['OBE ITEM','DESCRIPTION','CATEGORY','MATERIAL']
+cols = ['Product Name',	'Product Category',	'Document Type',	'Status',	'Document Info #1',	'Document Info #2',\
+        	'Filename',	'Page Info #1',	'Page Info #2',	'Page #',	'Text',	'Filepath', 'Count']
+for col_num, txt in enumerate(cols):
+    worksheet.write(0,col_num,txt)
+#plFile = 'Parts_List_Simple.xlsx'
+
+pdfList = []
+bolList = []
+dtlList = []
+row = 1
+ct = -1
+#for ket in range(1):
+for path, catDirs, files in os.walk(pdfDir):
+    #for path, prodDirs, files in 
+    for pdfFile in files:
+        ct += 1
+        
+        ln1 = ''
+        ln2 = ''
+        if pdfFile.endswith('pdf'):
+            ff = False
+            pdfList.append(pdfFile)
+            # print whole path of files
+            pgNm = []
+            pdNm = []
+            pdf = fitz.open(os.path.join(path,pdfFile))
+            pg = 0
+            for page in pdf:
+                mf = -1
+                mf2 = -1
+                mf3 = -1
+                pg += 1
+                maxFnt = 0
+                i = 0
+                
+                results = [] # list of tuples that store the information as (text, font size, font name) 
+                dict = page.get_text("dict")
+                blocks = dict["blocks"]
+                for block in blocks:
+                    if "lines" in block.keys():
+                        spans = block['lines']
+                        for span in spans:
+                            data = span['spans']  
+                            for lines in data:
+                                #if keyword in lines['text'].lower(): # only store font information of a specific keyword
+                                lineStr = lines['text'].replace('®','')
+                                lineStr = lineStr.replace('™','')
+                                
+                                if "BuildingEnvelope" in lines['text'] or lines['text'] == 'Table of Contents':
+                                    be = True
+                                #elif '"' in lines['text']:
+                                #    be = False
+                                #elif '/' in lines['text']:
+                                #    be = False
+                                #elif '®' in lines['text']:
+                                #    be = False
+                                #elif '™' in lines['text']:
+                                #    be = False
+                                elif pg == 1 and '®' in lines['text']:
+                                    be = False
+                                elif lines['text'] == '':
+                                    be = False
+                                else:
+                                    results.append((lines['text'], lines['size'], lines['font']))                        
+                                        
+                                    if lines['size'] > maxFnt:
+                                        maxFnt = lines['size']
+                                        mf = i
+                                        mf2 = -1
+                                        mf3 = -1
+                                        if pg == 1:
+                                            ln1 = lines['text']
+                                        # lines['text'] -> string, lines['size'] -> font size, lines['font'] -> font name
+                                    elif lines['size'] == maxFnt:
+                                        if mf2 == -1:
+                                           mf2 = i
+                                        elif mf3 == -1:
+                                            mf3 = i
+                                        if pg == 1:
+                                            ln2 = lines['text'] 
+                                    i += 1
+                if pg > 2 or True:              
+                    #for prt in df['OBE ITEM']:
+                    for j in range(len(results)):
+                        #if str(prt) in str(results[j]):  
+                        fldr = path.split('\\')
+                        #if prt in str(results[j]): 
+                        #doc_list.append(fldr[-1])
+                        #prod_list.append(fldr[-2])
+                        #cat_list.append(fldr[-3])
+                            #print('We found a part')
+                        #---product name---
+                        worksheet.write(row, 0, fldr[-2])
+                        #---product category---
+                        worksheet.write(row, 1, fldr[-3])
+                        #---document type---
+                        worksheet.write(row, 2, fldr[-1])
+                        #---Product status---
+                        worksheet.write(row, 3, fldr[-4])
+
+                        worksheet.write(row, 4, ln1) 
+                        worksheet.write(row, 5, ln2) 
+                        worksheet.write(row, 6, pdfFile) 
+                        worksheet.write(row, 7, results[mf][0]) 
+                        if mf2 >= 0:
+                            worksheet.write(row, 8, results[mf2][0]) 
+                        #if mf3 >= 0:
+                        #    worksheet.write(row, 9, results[mf3][0]) 
+                        worksheet.write(row, 9, pg) 
+                        worksheet.write(row,10,results[j][0])
+                        worksheet.write(row,11,path)
+                        worksheet.write(row,12,1)
+
+                        #for k in range(len(cols)):
+                        #    worksheet.write(row,col,df[cols[k]].loc[(df['OBE ITEM'] == prt)].values[0])
+                        #    col += 1
+                        row += 1               
+                    
+                #pdNm.append(results[mF][0])
+                #pgNm.append(results[mF2][0])
+
+            pdf.close()
+            bolList.append(ff)
+                                
+workbook.close()
+print('PDF reading complete')
+
+#---Open the excel file and write the pickle file needed for streamlit---
+#---Note: it would be quicker to store all the data as a dataframe
+#----to begin with and then write both an xlsx and sav at the end
+#----but the code was originally written to save an excel file
+#----it would need to be re-written somewhat
+if wrtPkl:
+    df1 = pd.read_excel(fOut)
+    df1['Text'].replace('',np.nan, inplace=True)
+    df1 = df1.dropna(subset=['Text'])
+    pklFile = fName + ".sav"
+    df1.to_pickle(pklFile)
+
+toc = time.perf_counter()
+
+print(f"Run time {toc - tic:0.4f} seconds")
